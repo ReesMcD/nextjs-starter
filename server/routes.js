6668514952c6d999b1a0c5aed207df26d31f2131
Rees McDevitt <mcdevittrees@gmail.com>
2019-08-21 23:52:54 -0400

    clean up

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,8 @@
 let routes = require('next-routes');
 
-module.exports = routes()
-  .add({ name: 'posts', pattern: '/posts/:id', page: 'posts' })
-  .add({ name: 'user', pattern: '/user/:id', page: 'user/user' });
+// Add Routes
+module.exports = routes().add({
+  name: 'user',
+  pattern: '/user/:id',
+  page: 'user/user'
+});
